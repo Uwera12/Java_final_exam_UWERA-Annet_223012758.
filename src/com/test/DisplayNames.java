7025e5de4149f27dbc1618b0c8bd64beedf8c04d
Uwera12 <annetuwera307@gmail.com>
2025-12-22 00:58:21 -0800

    Update DisplayNames.java

--- src/com/test/DisplayNames.java
+++ src/com/test/DisplayNames.java
@@ -3,6 +3,4 @@ package com.test;
 public class DisplayNames {
     public static int CurrentUserId;
     public static String CurrentUserFullName;
-    public static String CurrentUserEmail;
-    public static String CurrentUserRole;
 }
